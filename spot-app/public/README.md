8c57c08d8f40e769573774fd1befa78edbaca8f9
Evelyn <student_104@ool-ad0202f5.dyn.optonline.net>
2018-11-15 11:42:45 -0500

    changed to petfind

---
+++ spot-app/public/README.md
@@ -0,0 +1,4 @@
+## SPOT
+
+- The app for finding your new best friend forever in the NYC area
+- SPOT is an adoption center website that lets users search for NYC dogs in need of forever homes
--- spot-app/src/App.js
+++ spot-app/src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import jsonpAdapter from 'axios-jsonp';
+
+const URL = 'http://api.petfinder.com/pet.find';
+const KEY = process.env.REACT_APP_API_KEY;
+const PARAMS = {
+  key: KEY,
+  format: 'json',
+  animal: 'dog',
+  location: 'New York City, NY' //only want to show results for NYC since this is going to be local shelter
+};
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      breeds: []
+    }
+  }
+
+  componentDidMount() {
+    this.fetch();
+  }
+
+  async fetch() {
+    const opts = {
+      params: PARAMS,
+      adapter: jsonpAdapter,
+    };
+
+    try {
+
+    const resp = await axios(URL, opts);
+      const data = resp.data;
+      const breedList = data.petfinder.breeds.breed;
+      const breeds = breedList.map(breed => Object.values(breed)[0]);
+      this.setState({ breeds: breeds });
+    } catch(e) {
+      console.log(e);
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h2>Find your new BFF</h2>
+        {this.state.breeds.map(breed => (
+          <div key={breed}>{breed}</div>
+        ))}
       </div>
     );
   }
