acd7ec35c55b11b09360abfd237547288c358b6f
Evelyn Lugo <evelynlugo@Evelyns-Air.fios-router.home>
2019-10-08 18:40:12 -0400

    updated styling

--- spot-app/src/Components/PetResults.js
+++ spot-app/src/Components/PetResults.js
@@ -5,16 +5,18 @@ export default function PetResults(props) {
   return (
     <div className='pet-main-profile'>
       {props.details.map(x => {
-      return( <div className='pet-main-details'>
-        <img className='photo' src={x.photo} alt='dog'/>
-          <div className='pet-details-menu'>
-            <p className='pet-details pet-name'>{x.name}</p>
-            <p className='pet-details'>{x.description}</p>
-            <p className='pet-details'>{x.age}</p>
-            <p className='pet-details'>{x.breed}</p>
-            <p className='pet-details'>{x.sex}</p>
-          </div>
-      </div>)
+      return ( 
+          <div className='pet-main-details'>
+            <img className='photo' src={x.photo} alt='dog'/>
+              <div className='pet-details-menu'>
+                <p className='pet-details pet-name'>{x.name}</p>
+                <p className='pet-details'>{x.age}</p>
+                <p className='pet-details'>{x.breed}</p>
+                <p className='pet-details'>{x.sex}</p>
+                <p className='pet-details'>{x.description}</p> 
+              </div>
+        </div>
+      )
       })}
     </div>
   );
